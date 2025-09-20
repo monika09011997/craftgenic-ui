ccff9ce7a5a43e0463d9c77344a6ff5d59edd684
“monika09011997” <monikakumari9197@gmail.com>
2025-09-20 16:55:49 +0530

    folder structure changed

--- src/home/api.ts
+++ src/home/api.ts
@@ -4,11 +4,11 @@ import { ProductListItem } from "./types";
 
 export const fetchProductsList = async (): Promise<ProductListItem[]> => {
     try {
-        const response = await axios.get<ProductListItem[]>(
-            "/api/products"
-        );
+        const response = await axios.get<ProductListItem[]>("/api/products");
         return response.data;
     } catch (error) {
-        throw error;
+        // Here, we are doing something useful with the error.
+        console.error("Failed to fetch products:", error); 
+        throw error; // Then we re-throw it.
     }
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
 
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import './index';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { persistor, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <Provider store={store}>
               <PersistGate loading={null} persistor={persistor}>
@@ -24,4 +23,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
